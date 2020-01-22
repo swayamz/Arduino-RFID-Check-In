5fdd53c65f83d7daa6d7c939c39cbdda36bec0b2
swayamz <swayamsaraiya@gmail.com>
2020-01-22 14:33:04 -0800

    Add files via upload

---
+++ sketch_jan21a.ino
@@ -0,0 +1,80 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN 10
+#define RST_PIN 9
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+
+void setup()
+{
+  pinMode(2, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(3, OUTPUT); // set a pin for buzzer output
+  Serial.begin(9600);   // Initiate a serial communication
+  SPI.begin();      // Initiate  SPI bus
+  mfrc522.PCD_Init();   // Initiate MFRC522
+}
+
+void buzz(int targetPin, long frequency, long length) {
+  long delayValue = 1000000/frequency/2; // calculate the delay value between transitions
+  // 1 second's worth of microseconds, divided by the frequency, then split in half since
+  // there are two phases to each cycle
+  long numCycles = frequency * length/ 1000; // calculate the number of cycles for proper timing
+  // multiply frequency, which is really cycles per second, by the number of seconds to 
+  // get the total number of cycles to produce
+ for (long i=0; i < numCycles; i++){ // for the calculated length of time...
+    digitalWrite(targetPin,HIGH); // write the buzzer pin high to push out the diaphram
+    delayMicroseconds(delayValue); // wait for the calculated delay value
+    digitalWrite(targetPin,LOW); // write the buzzer pin low to pull back the diaphram
+    delayMicroseconds(delayValue); // wait again for the calculated delay value
+  }
+}
+void loop() 
+{
+  // Look for new cards
+  digitalWrite(4, HIGH);
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+  //Show UID on serial monitor
+  Serial.print("UID tag :");
+  String content= "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.print(mfrc522.uid.uidByte[i], HEX);
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  Serial.println();
+  Serial.print("Message : ");
+  content.toUpperCase();
+  if (content.substring(1) == "8B CC A9 89") //change here the UID of the card/cards that you want to give access
+  {
+    Serial.println("Authorized access");
+    Serial.println();
+    digitalWrite(4, LOW);
+    digitalWrite(2, HIGH);
+    buzz(3, 2500, 500); // buzz the buzzer on pin 4 at 2500Hz for 500 milliseconds
+    delay(2000);
+    digitalWrite(2, LOW);
+  }
+ 
+ else   {
+    Serial.println(" Access denied");
+    for (int i = 0; i<3; i++){
+      buzz(3, 100, 500); // buzz the buzzer on pin 4 at 2500Hz for 500 milliseconds
+      digitalWrite(4, LOW);
+      delay(1000);
+      digitalWrite(4, HIGH);
+      delay(1000);
+    }
+  }
+} 
